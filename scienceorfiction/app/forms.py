8774a21e6767676f36db4c8dd0f5f060ddc8b976
Michael Cole <mcole042891@gmail.com>
2020-02-06 12:47:48 -0600

    [US0026]: Admin Create page updated

--- scienceorfiction/app/forms.py
+++ scienceorfiction/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import InputRequired, ValidationError
+from wtforms.validators import InputRequired, ValidationError, EqualTo
 
 
 def adminAlreadyExists(form, field):
@@ -44,7 +44,13 @@ class AdminCreateForm(FlaskForm):
         adminAlreadyExists
     ])
 
-    password = PasswordField('Password')
+    password = PasswordField('Password', validators=[
+        EqualTo('passwordConfirm', message='Passwords do not match.')
+    ])
+
+    passwordConfirm = PasswordField('Confirm Password', validators=[
+        EqualTo('password', message='Passwords do not match.')
+    ])
 
     submit = SubmitField('Create Account')
 
