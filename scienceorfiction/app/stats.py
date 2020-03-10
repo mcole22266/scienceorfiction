875880e80dbca0a9c923794bb87bd9b9714e2c63
Michael Cole <mcole042891@gmail.com>
2020-03-10 02:30:07 -0500

    [US0073]: Added some code commenting to Python files

--- scienceorfiction/app/stats.py
+++ scienceorfiction/app/stats.py
@@ -1,12 +1,29 @@
+# Used to collect useful stats to be displayed by Bokeh graphs
+
 from .extensions import getAllEpisodes, getEpisode, getParticipant, getResults
 
 
 def getRogueOverallAccuracy(roguename, daterange=False, theme=False):
+    '''
+    Used to get a single overall accuracy for a given range. Given a
+    date range or theme, more specific accuracies can be found.
+
+    Args:
+        roguename (str): Name of the rogue stats should be gathered for.
+        daterange (list or tuple) - optional: Start and end dates for more
+            specific accuracy information
+        theme (str) - optional: Theme for more specific accuracy information.
+    Returns:
+        (tuple): (Accuracy, Total Correct, Total Incorrect)
+    '''
     rogue = getParticipant(roguename)
+    # get all results
     results = getResults(participant_id=rogue.id, daterange=daterange,
                          theme=theme)
+    # get only results where rogue was present
     presentResults = [result for result in results
                       if not result.is_absent and not result.is_presenter]
+    # get only results where rogue was correct
     correctResults = [result for result in presentResults
                       if result.is_correct]
     totalCorrect = len(correctResults)
@@ -20,7 +37,21 @@ def getRogueOverallAccuracy(roguename, daterange=False, theme=False):
 
 
 def getRogueAccuracy(roguename, daterange=False, theme=False):
+    '''
+    Used to get a list of accumulated accuracy over time for a
+    given rogue.
+
+    Args:
+        roguename (str): Name of the rogue stats should be gathered for.
+        daterange (list or tuple) - optional: Start and end dates for more
+            specific accuracy information
+        theme (str) - optional: Theme for more specific accuracy information.
+    Returns:
+        (list): list of accumulated accuracies over time.
+
+    '''
     rogue = getParticipant(roguename)
+    # get all results
     results = getResults(participant_id=rogue.id, daterange=daterange,
                          theme=theme)
     accuracies = []
@@ -40,6 +71,16 @@ def getRogueAccuracy(roguename, daterange=False, theme=False):
 
 
 def getRogueAttendance(roguename, daterange=False):
+    '''
+    Used to get overall attendance of rogues.
+
+    Args:
+        roguename (str): Name of the rogue stats should be gathered for.
+        daterange (list or tuple) - optional: Start and end dates for more
+            specific attendence information
+    Returns:
+        (float): Overall attendence percentage.
+    '''
     rogue = getParticipant(roguename)
     results = getResults(participant_id=rogue.id, daterange=daterange)
     present = [result for result in results if not result.is_absent]
@@ -51,6 +92,23 @@ def getRogueAttendance(roguename, daterange=False):
 
 def getSweeps(allSweeps=False, presenter=False,
               participant=False, daterange=False):
+    '''
+    Used to get a list of episodes that were sweeps of
+    some sort.
+
+    Args:
+        allSweeps (bool) - optional: If True, both
+            presenter and participant sweeps will
+            be gathered.
+        presenter (bool) - optional: If True, only
+            presenter sweeps will be gathered.
+        participants (bool) - optional: If True, only
+            participant sweeps will be gathered.
+        daterange (list or tuple) - optional: Start and end dates for more
+            specific attendence information
+    Returns:
+        (list): A list of all episodes that fit the optional criteria
+    '''
     if allSweeps:
         presenter = True
         participant = True
@@ -59,6 +117,8 @@ def getSweeps(allSweeps=False, presenter=False,
     for episode in episodes:
         results = getResults(episode_id=episode.id)
         for result in results:
+            # remove results where participant is absent
+            # or presenter
             if result.is_correct is None:
                 results.remove(result)
         totalCorrect = 0
--- scienceorfiction/config.py
+++ scienceorfiction/config.py
@@ -4,7 +4,9 @@ from os import environ, urandom
 class Config:
     '''
     Python representation of environment variables
-    to be used within the app
+    to be used within the app. If a new environment
+    variable is added in an env file, it likely needs
+    to also be added here to be used by Flask.
     '''
 
     # flask settings
